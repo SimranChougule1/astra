0446956497c9d027f47f80ec317edfc52ea5cfae
imnavanath <navanathb@bsf.io>
2021-09-09 19:59:21 +0530

    chore: lint error resolving

--- tests/e2e/specs/customizer/header-builder/Site-identity/margin.test.js
+++ tests/e2e/specs/customizer/header-builder/Site-identity/margin.test.js
@@ -5,27 +5,27 @@ describe( 'Site Identity margin settings in the customizer', () => {
 	it( 'spacing should apply corectly', async () => {
 		const siteTaglineSpacing = {
 			'title_tagline-margin': {
-				'desktop'    : {
-					top    : 10,
-					right  : 15,
+				desktop : {
+					top : 10,
+					right : 15,
 					bottom : 20,
-					left   : 25,
+					left : 25,
 				},
-				'tablet'     : {
-					top    : '',
-					right  : '',
+				tablet : {
+					top : '',
+					right : '',
 					bottom : '',
-					left   : '',
+					left : '',
 				},
-				'mobile'     : {
-					top    : '',
-					right  : '',
+				mobile : {
+					top : '',
+					right : '',
 					bottom : '',
-					left   : '',
+					left : '',
 				},
 				'desktop-unit' : 'px',
-				'tablet-unit'  : 'px',
-				'mobile-unit'  : 'px',
+				'tablet-unit' : 'px',
+				'mobile-unit' : 'px',
 			},
 		};
 
@@ -41,28 +41,28 @@ describe( 'Site Identity margin settings in the customizer', () => {
 			selector: '.ast-builder-layout-element .ast-site-identity',
 			property: 'margin-top',
 		} ).cssValueToBe(
-			`${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop' ].top }${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop-unit' ] }`,
+			`${ siteTaglineSpacing[ 'title_tagline-margin' ].desktop.top }${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop-unit' ] }`,
 		);
 
 		await expect( {
 			selector: '.ast-builder-layout-element .ast-site-identity',
 			property: 'margin-right',
 		} ).cssValueToBe(
-			`${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop' ].right }${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop-unit' ] }`,
+			`${ siteTaglineSpacing[ 'title_tagline-margin' ].desktop.right }${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop-unit' ] }`,
 		);
 
 		await expect( {
 			selector: '.ast-builder-layout-element .ast-site-identity',
 			property: 'margin-bottom',
 		} ).cssValueToBe(
-			`${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop' ].bottom }${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop-unit' ] }`,
+			`${ siteTaglineSpacing[ 'title_tagline-margin' ].desktop.bottom }${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop-unit' ] }`,
 		);
 
 		await expect( {
 			selector: '.ast-builder-layout-element .ast-site-identity',
 			property: 'margin-left',
 		} ).cssValueToBe(
-			`${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop' ].left }${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop-unit' ] }`,
+			`${ siteTaglineSpacing[ 'title_tagline-margin' ].desktop.left }${ siteTaglineSpacing[ 'title_tagline-margin' ][ 'desktop-unit' ] }`,
 		);
 
 	} );
