abeaef54ebed10f12a6738d2af4935288c75a860
Simran <simranc@bsf.io>
2021-09-18 17:20:58 +0530

    Added responsive value in font size

--- tests/e2e/specs/customizer/footer-copyright/footer-copyright-font-size.test.js
+++ tests/e2e/specs/customizer/footer-copyright/footer-copyright-font-size.test.js
@@ -1,10 +1,19 @@
 import { createURL } from "@wordpress/e2e-test-utils";
 import { setCustomize } from '../../../utils/set-customize';
+import {__experimentalScrollable}from  '@wordpress/e2e-test-utils';
 describe( 'copyright font size settings in the customizer', () => {
     it( 'copyright font size should apply correctly', async () => {
         const copyrightfontsize = {
 
-            'font-size-section-footer-copyright':'40px',
+            'font-size-section-footer-copyright':
+            {
+                desktop: '20',
+                tablet: '20',
+                mobile: '20',
+                'desktop-unit': 'px',
+                'tablet-unit': 'px',
+                'mobile-unit': 'px',
+            },
         };
 
         await setCustomize( copyrightfontsize );
@@ -16,7 +25,7 @@ describe( 'copyright font size settings in the customizer', () => {
         await expect( {
             selector: '.ast-footer-copyright',
             property: 'font-size',
-        } ).cssValueToBe(`${ copyrightfontsize [ 'font-size-section-footer-copyright' ] }`,);
+        } ).cssValueToBe(`${ copyrightfontsize [ 'font-size-section-footer-copyright' ].desktop }${ copyrightfontsize [ 'font-size-section-footer-copyright' ]['desktop-unit'] }`,);
 
     });
 })
