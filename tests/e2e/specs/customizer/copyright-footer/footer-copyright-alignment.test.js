dc16db08650a7b804d479897442e01b3b4787b1e
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2021-09-22 12:21:24 +0530

    Update footer-copyright-alignment.test.js

--- tests/e2e/specs/customizer/copyright-footer/footer-copyright-alignment.test.js
+++ tests/e2e/specs/customizer/copyright-footer/footer-copyright-alignment.test.js
@@ -3,13 +3,13 @@ import { setCustomize } from '../../../utils/set-customize';
 describe( 'copyright alignment settings in the customizer', () => {
     it( 'copyright alignment should apply correctly', async () => {
             	 const copyrightAlignment = {
-                'footer-copyright-alignment':'center',
+                 'footer-copyright-alignment':'center',
             };
 
             await setCustomize( copyrightAlignment );
             await page.goto( createURL( '/' ), {
                 waitUntil: 'networkidle0',
-                } );
+            } );
 
             await page.evaluate( () => {
                 window.scrollBy(0, window.innerHeight);
