aa1ed36f38d9a8cbbaedb82e8c738af96f6a975c
Simran <simranc@bsf.io>
2021-09-24 16:12:23 +0530

    Test case for single post content width

---
+++ tests/e2e/specs/customizer/Blog/single-post-content-width.test.js
@@ -0,0 +1,49 @@
+import { createURL,createNewPost,publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/set-customize';
+describe( ' Single post content width settings in the customizer', () => {
+	it( 'Single post default width should apply correctly', async () => {
+        const contentwidth = {
+            'blog-single-width': 'default',
+        };
+        await setCustomize( contentwidth );
+        await createNewPost( {
+            postType: 'post',
+            title: 'sample-page',
+        } );
+        await publishPost();
+
+        await page.goto( createURL( 'sample-page' ), {
+            waitUntil: 'networkidle0',
+        } );
+        await page.waitForSelector('.site-content .ast-container');
+        await expect( {
+            selector: '.site-content .ast-container',
+            property: ' ',
+        } ).cssValueToBe(``); 
+
+    });
+})
+
+it( 'Blog Archive custom width should apply correctly', async () => {
+    const contentwidth = {
+        'blog-single-max-width': '1200',
+    };
+    await setCustomize( contentwidth );
+    await createNewPost( {
+        postType: 'post',
+        title: 'sample-page',
+        content:'blog post created',
+    } );
+    await publishPost();
+
+    await page.goto( createURL( 'sample-page' ), {
+        waitUntil: 'networkidle0',
+    } );
+    await page.waitForSelector('.single-post .site-content > .ast-container');
+    await expect( {
+        selector: '.single-post .site-content > .ast-container',
+        property: '',
+    } ).cssValueToBe(``); 
+    
+
+});
