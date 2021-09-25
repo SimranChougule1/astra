782f0e0a36429dacede050ce990ae83acd37b23e
Simran <simranc@bsf.io>
2021-09-25 18:42:51 +0530

    Test case for copyright alignment

---
+++ tests/e2e/specs/customizer/Copyright/copyright-alignment.test.js
@@ -0,0 +1,27 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/set-customize';
+describe( 'copyright alignment settings in the customizer', () => {
+    it( 'copyright alignment should apply correctly', async () => {
+            	 const copyrightAlignment = {
+                 'footer-copyright-alignment':{
+                    'desktop':'left',
+                    'tablet':'left',
+                    'mobile':'left',
+                }
+            };
+
+            await setCustomize( copyrightAlignment );
+            await page.goto( createURL( '/' ), {
+                waitUntil: 'networkidle0',
+            } );
+
+            await page.evaluate( () => {
+                window.scrollBy(0, window.innerHeight);
+            });
+            await page.waitForSelector( '.ast-builder-layout-element' );
+            await expect( {
+                selector: '.ast-builder-layout-element',
+                property: '',
+            } ).cssValueToBe( ``);
+
+}); });
