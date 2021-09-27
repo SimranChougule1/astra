5c460cfad6e016fbccb78f4deafdf8880a6e531e
Simran <simranc@bsf.io>
2021-09-27 17:42:25 +0530

    added responsive in copyright margin but getting error

--- tests/e2e/specs/customizer/Copyright/copyright-margin.test.js
+++ tests/e2e/specs/customizer/Copyright/copyright-margin.test.js
@@ -1,8 +1,8 @@
 import { createURL } from "@wordpress/e2e-test-utils";
 import { setCustomize } from '../../../utils/set-customize';
 describe(' Copyright Margin setting in customizer', () => {
-    it( 'top menu margin style should apply correctly', async () => {
-		const  copyrightmargin= {
+    it( 'Copyright margin style should apply correctly', async () => {
+		const copyrightmargin= {
             'section-footer-copyright-margin': {
                 'desktop': {
                     top: 50,
@@ -22,23 +22,92 @@ describe(' Copyright Margin setting in customizer', () => {
                     bottom: 50,
                     left: 50, 
                 },
+                
                 'desktop-unit': 'px',
 				'tablet-unit': 'px',
-				'mobile-unit': 'px',
+            	'mobile-unit': 'px',
+                
             },
+            
         };
         await setCustomize(  copyrightmargin );
 
         await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
+
+        await page.evaluate( () => {
+            window.scrollBy(0, window.innerHeight);
+        });
+
         await page.waitForSelector( '.ast-footer-copyright' );
 
+
+        //for Desktop
         await expect( {
 			selector: '.ast-footer-copyright',
-			property: '',
-		} ).cssValueToBe(``,
-			//`${  copyrightmargin[ 'section-footer-copyright-margin' ]}`,
-		);
+			property: 'margin-top',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].desktop }${ copyrightmargin [ 'section-footer-copyright-margin' ]['desktop-unit'] }`,);
+		
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-right',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].desktop }${ copyrightmargin [ 'section-footer-copyright-margin' ]['desktop-unit'] }`,);
+		
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-bottom',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].desktop }${ copyrightmargin [ 'section-footer-copyright-margin' ]['desktop-unit'] }`,);
+		
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-left',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].desktop }${ copyrightmargin [ 'section-footer-copyright-margin' ]['desktop-unit'] }`,);
+		
+        
+        //for Tablet
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-top',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].tablet }${ copyrightmargin [ 'section-footer-copyright-margin' ]['tablet-unit'] }`,);
+		
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-right',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].tablet }${ copyrightmargin [ 'section-footer-copyright-margin' ]['tablet-unit'] }`,);
+		
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-bottom',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].tablet }${ copyrightmargin [ 'section-footer-copyright-margin' ]['tablet-unit'] }`,);
+		
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-left',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].tablet }${ copyrightmargin [ 'section-footer-copyright-margin' ]['tablet-unit'] }`,);
+
+        //for Mobile
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-top',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].mobile }${ copyrightmargin [ 'section-footer-copyright-margin' ]['mobile-unit'] }`,);
+		
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-right',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].mobile }${ copyrightmargin [ 'section-footer-copyright-margin' ]['mobile-unit'] }`,);
+		
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-bottom',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].mobile }${ copyrightmargin [ 'section-footer-copyright-margin' ]['mobile-unit'] }`,);
+		
+        await expect( {
+			selector: '.ast-footer-copyright',
+			property: 'margin-left',
+		} ).cssValueToBe(`${ copyrightmargin [ 'section-footer-copyright-margin' ].mobile }${ copyrightmargin [ 'section-footer-copyright-margin' ]['mobile-unit'] }`,);
 	} );
-} );
+})
+
+
+
